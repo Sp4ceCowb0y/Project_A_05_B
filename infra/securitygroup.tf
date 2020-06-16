6de437eaa4e88133fb2f28eaa27c1ec32e8c651b
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2020-06-16 17:02:07 +0200

    securitygroup.tf

--- infra/securitygroup.tf
+++ infra/securitygroup.tf
@@ -41,19 +41,19 @@ resource "aws_security_group" "app-securitygroup" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["79.155.127.21/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["79.155.127.21/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
     ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["79.155.127.21/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "app-securitygroup"
